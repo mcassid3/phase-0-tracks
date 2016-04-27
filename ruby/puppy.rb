545bbffcf02692e6ceaabfe67b4ae1a8eef48743
mcassid3 <mcassid3@gmail.com>
2016-04-26 21:37:28 -0500

    defined a puppy class with attributes and method names

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+#CLASS ------------------------
+#Puppy
+
+
+#CHARACTERISTICS ----------------
+#Weight: varies
+#Color: varies
+#Paws: 4
+#Tails: 1
+#Likes_being_pet: Yes
+
+
+#BEHAVIOR -----------------------
+#Runs
+#Barks
+#Sits
+#Lays
+
+
