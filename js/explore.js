36444b436067c417d2914ff0d56b57613fca34db
mcassid3 <mcassid3@gmail.com>
2016-05-07 20:21:46 -0500

    pseudocode the reverse function

---
+++ js/explore.js
@@ -0,0 +1,15 @@
+// input: a string ("hello")
+  //determine the index of each letter (h = 0, e = 1, l = 2, l = 3, o = 4)
+  //determine the length of the word (5)
+  //loop through each letter in the word hello
+    //subtract: length of word (5) - index position of letter
+    //the return of this value = the new index position for that letter
+      //h = index 0 
+      //5 - 0 = 5
+      //now h = index 5
+      //e = index 1
+      //5 - 1 = 4
+      // now e = index 4
+    //print the new string, with the letters now in their new indices
+
+var word
