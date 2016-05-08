c7202a3e216706fdf2c6eb254a2262d5fc3067df
mcassid3 <mcassid3@gmail.com>
2016-05-08 09:53:46 -0500

    add arrays for horse names and colors

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+//Declare array variables
+var colors = ["blue","red","orange","yellow"];
+var names = ["Crazy Legs","Triple Frown","Mane Attraction","Strutz"];
+
+//Add a color and a horse to arrays
+colors.push("green");
+names.push("Uncle Buck");
+
+//Print arrays
+console.log(colors)
+console.log(names)
